958eb28ba134446fbaf89a452de7ed29e1beed22
Gabriel <gabrielandre699@gmail.com>
2023-07-07 12:44:50 -0300

    Initial Commit

---
+++ Ui-Demo/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  Ui-Demo
+//
+//  Created by Gabriel André Barreto Olimpio dos Santos on 07/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("IMG_4642")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack{
+                    
+                    Text("Praia dos Presos")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                }
+                
+
+                
+                Text("Venha visitar essa praia muito doida.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+            }
+            .padding() // Padding dentro do background
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15))
+            .padding() // Padding fora do background
+        }
+        
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ui-Demo/Ui_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ui_DemoApp.swift
+//  Ui-Demo
+//
+//  Created by Gabriel André Barreto Olimpio dos Santos on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Ui_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
